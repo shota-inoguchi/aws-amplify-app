0783c4f3bd87b528ee48a0969b592e515f8e054f
unknown <230450094@kccs.auth>
2023-08-21 17:17:21 +0900

    mod

--- src/App.js
+++ src/App.js
@@ -6,13 +6,10 @@ const App = () => {
   // 仮のデータ
   const devices1 = [
     { deviceID: '123', deviceName: 'Device 1', pressure: 1013, hat: 200 },
-    { deviceID: '456', deviceName: 'Device 2', pressure: 1005, hat: 150 },
+    { deviceID: '456', deviceName: 'Device 2', pressure: 1005, hat: 150 },// 他のデバイスデータ
   ];
-    // 他のデバイスデータ
-    const devices2 = [
-      { deviceID: '123', deviceName: 'Device 1', pressure: 1013, hat: 200 },
-      { deviceID: '456', deviceName: 'Device 2', pressure: 1005, hat: 150 },
-  ];
+    
+
 
   return (
     <div className="App">
