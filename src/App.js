52cc624b7bac49db34579e0d74411e05d3eb701c
unknown <230450094@kccs.auth>
2023-08-21 16:53:58 +0900

    mod

--- src/App.js
+++ src/App.js
@@ -1,33 +1,9 @@
 import React from 'react';
 import DeviceCard from './DeviceCard';
 
-const App = () => {
-  const deviceData = [
-    {
-      deviceId: '123456',
-      deviceName: 'デバイスA',
-      pressure: 1013.25,
-      groundHeight: 100
-    },
-    // 他のデバイス情報
-  ];
 
-  return (
-    <div>
-      {deviceData.map((device, index) => (
-        <DeviceCard
-          key={index}
-          deviceId={device.deviceId}
-          deviceName={device.deviceName}
-          pressure={device.pressure}
-          groundHeight={device.groundHeight}
-        />
-      ))}
-    </div>
-  );
-};
 
-export default App;
+
 
 const AWS = require('aws-sdk');
 
@@ -49,3 +25,5 @@ dynamoDB.scan(params, (err, data) => {
     console.log('データの取得に成功:', JSON.stringify(data, null, 2));
   }
 });
+
+export default App;
