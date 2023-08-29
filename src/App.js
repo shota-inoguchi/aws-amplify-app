c283a5cbb99ed21e6435d93f0062baf1c198ddf1
unknown <230450094@kccs.auth>
2023-08-29 10:54:10 +0900

    deploy

--- src/App.js
+++ src/App.js
@@ -1,34 +1,64 @@
 import React from 'react';
 import DeviceList from './DeviceList';  // 適切なパスに変更してください
 
-function App() {
-    const devices = [
-        {
-            id: '1',
-            name: 'Device 1',
-            pressure: '1013 hPa',
-            altitude: '100 m'
-        },
-        {
-            id: '2',
-            name: 'Device 2',
-            pressure: '1010 hPa',
-            altitude:'105 m' 
-        },
-        {
-            id: '3',
-            name: 'Device 3',
-            pressure: '1024 hPa',
-            altitude: '101 m'
-        },
-        // ... 他のデバイスデータ
-    ];
+
+import React, { useEffect, useState } from 'react';
+import AWSAppSyncClient from 'aws-appsync';
+import { graphql } from 'graphql-tag';
+
+const GET_DEVICES = graphql`
+    query ListDevices {
+        listDevices {
+            id
+            name
+            pressure
+            altitude
+        }
+    }
+`;
+
+function DeviceList() {
+    const [devices, setDevices] = useState([]);
+
+    useEffect(() => {
+        const client = new AWSAppSyncClient({
+            url: 'https://jvgy2cnbljeopbngyjyv5zxzw4.appsync-api.ap-northeast-1.amazonaws.com/graphql',
+            region: 'ap-northeast1',
+            
+        });
+
+        client.query({ query: GET_DEVICES }).then(response => {
+            setDevices(response.data.listDevices);
+        });
+    }, []);
+
+    
+    useEffect(() => {
+        client.query({ query: GET_DEVICES }).then(response => {
+            setDevices(response.data.listDevices);
+        });
+    }, []);
 
     return (
-        <div className="container mt-5">
-            <DeviceList devices={devices} />
+        <div className="d-flex flex-wrap justify-content-around">
+            {devices.map(device => (
+                <DeviceCard key={device.id} device={device} />
+            ))}
         </div>
     );
 }
 
-export default App;
+const DeviceCard = ({ device }) => {
+    return (
+        <div className="card mb-3" style={{ maxWidth: '20rem' }}>
+            <div className="card-header">ID: {device.id}</div>
+            <div className="card-body">
+                <h5 className="card-title">{device.name}</h5>
+                <p className="card-text">気圧: {device.pressure}</p>
+                <p className="card-text">地上高: {device.altitude}</p>
+            </div>
+        </div>
+    );
+};
+
+export default DeviceList;
